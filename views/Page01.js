e38d75e53fb8384526d1dc713ee9255c2c260f55
HoanganhIUH <21015461.anh@student.iuh.edu.vn>
2024-10-30 13:36:45 +0700

    Update Page01

--- views/Page01.js
+++ views/Page01.js
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from "react-native";
 export default function Page01(){
     return(
         <SafeAreaView style={{flex:1}}>
-            <Text>Page01</Text>
+            <Text>Page01345</Text>
         </SafeAreaView>
     )
 }
